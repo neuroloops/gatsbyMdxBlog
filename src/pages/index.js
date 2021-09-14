c4d3d43fd7dbad98f124c2d76e3374726509d8e7
ludo loops <ludoloops@protonmail.com>
2021-09-14 18:52:54 +0200

    feat: import mdx post

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,38 @@ import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  console.log(data)
   return (
     <Layout>
       <Hero showPerson />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        id
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          slug
+          date(formatString: "MMMM, Do YYYY")
+          image {
+            childrenImageSharp {
+              gatsbyImageData(layout: CONSTRAINED)
+            }
+          }
+        }
+        excerpt
+        body
+      }
+    }
+  }
+`
 
 export default IndexPage
