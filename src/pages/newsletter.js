9fd2a9df3b76caee19495c9f9b110ddb49c94adb
ludo loops <ludoloops@protonmail.com>
2021-09-14 16:11:10 +0200

    test: netlify form

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -9,8 +9,8 @@ const NewsLetter = () => {
           <h4>I write to my friends every few weeks</h4>
           <form
             className="contact-form"
-            name="testing-contact"
-            method="post"
+            name="contact-form"
+            method="POST"
             netlify-honeypot="bot-field"
             data-netlify="true"
             action="/success"
