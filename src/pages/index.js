69c1e3d8d8e974ea5af652d44b85b77524a046e9
ludo loops <ludoloops@protonmail.com>
2021-09-15 17:10:24 +0200

    feat: display posts on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,35 +3,39 @@ import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
+
 const IndexPage = ({ data }) => {
-  console.log(data)
+  const {
+    allMdx: { nodes: posts },
+  } = data
+
   return (
     <Layout>
       <Hero showPerson />
+      <Posts posts={posts} title="recently published" />
     </Layout>
   )
 }
 
 export const query = graphql`
   {
-    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+    allMdx(limit: 3, sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        id
+        excerpt
         frontmatter {
           title
           author
           category
-          readTime
-          slug
           date(formatString: "MMMM, Do YYYY")
+          slug
+          readTime
           image {
-            childrenImageSharp {
+            childImageSharp {
               gatsbyImageData(layout: CONSTRAINED)
             }
           }
         }
-        excerpt
-        body
+        id
       }
     }
   }
