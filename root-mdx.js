380bf824315bf42f1d3c67537da430b7f31e3aab
ludo loops <ludoloops@protonmail.com>
2021-09-17 03:58:53 +0200

    Feat: inline code

--- root-mdx.js
+++ root-mdx.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-import { Headings } from './src/components/Complete'
+import { Headings, Code } from './src/components/Complete'
 
 const components = {
   h2: Headings.myH2,
+  h4: Headings.myH4,
+  inlineCode: Code,
 }
 
 export const wrapMDX = ({ element }) => {
